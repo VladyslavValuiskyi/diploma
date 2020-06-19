926d727b9d31556d5d895c52faf0724cd48a43b2
Vladdos <vladyslav.valuiskyi@nure.ua>
2020-06-19 16:34:13 +0300

    Swagger version migration

--- api/src/main/java/com/proarea/api/controller/PictureController.java
+++ api/src/main/java/com/proarea/api/controller/PictureController.java
@@ -1,22 +1,12 @@
 package com.proarea.api.controller;
 
-import com.proarea.api.model.entity.UserEntity;
-import com.proarea.api.model.response.UserInfoResponse;
 import com.proarea.api.service.PictureService;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.Authorization;
 import org.springframework.http.*;
-import org.springframework.security.core.Authentication;
-import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import sun.misc.IOUtils;
-
-import javax.servlet.http.HttpServletResponse;
-import javax.ws.rs.Path;
-import java.io.*;
 
 @RestController
 public class PictureController {
